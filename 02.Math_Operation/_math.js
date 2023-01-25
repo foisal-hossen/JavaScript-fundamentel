0b43ea5408c95539c391906d302847bcae32b62e
foisal-hossen <farjana01811389904@gmail.com>
2023-01-25 11:41:26 +0600

    short hand update

---
+++ 02.Math_Operation/_math.js
@@ -0,0 +1,5 @@
+var price01 = 30;
+var price02 = 50;
+
+price01 = price01 + 20;
+console.log(price01); //ans: 50
---
+++ 02.Math_Operation/_shortHand.js
@@ -0,0 +1,20 @@
+var value01 = 50;
+value01 += 10;
+console.log(value01); //ans:60
+
+var value02 = 30
+value02 -= 10
+console.log(value02); //ans:20
+
+var num03 = 6
+num03 *= 5
+console.log(num03); //ans:30
+
+var age04 = 33;
+var sum = age04 /= 3
+console.log(sum);
+
+var num05 = 22;
+var count = 4;
+var total = num05 *= count;
+console.log(total); //ans: 88

6e11bb88b9385ec8895f973ba6b07cd883c16209
foisal-hossen <farjana01811389904@gmail.com>
2023-04-07 15:50:18 +0600

    data type

---
+++ 02.dataTypes/datatype.js
@@ -0,0 +1,15 @@
+let a = 10;     //ans: number
+let b = 5.3;    //ans: number
+let c = "hello";//ans: string
+let d = 'hi';   //ans: string
+let e = true;   //ans: boolean
+let f;          //ans: undefined
+let g = null;   //ans: object
+
+console.log(typeof a) 
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
